315fd1f8da078cc39d7e028f0d1d67fc7fc8d4d5
baQerial <mbapost@yahoo.com>
2021-05-25 21:32:44 +0430

    local component & style - slots

--- 7 and rest/src/App.vue
+++ 7 and rest/src/App.vue
@@ -1,35 +1,53 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
+	<div>
+		<!-- <TheHeader /> -->
+		<the-header></the-header>
+		<badge-list></badge-list>
+		<user-info
+			:full-name="activeUser.name"
+			:info-text="activeUser.description"
+			:role="activeUser.role"
+		></user-info>
+		<course-goals #default="slotProps">
+			<h2>{{ slotProps.item }}</h2>
+			<p>{{ slotProps.anotherProp }}</p>
+		</course-goals>
+	</div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
+
 export default {
-  data() {
-    return {
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
+	components: {
+		// "the-header": TheHeader,
+		// TheHeader: TheHeader,
+		TheHeader,
+		UserInfo,
+		BadgeList,
+		CourseGoals,
+	},
+	data() {
+		return {
+			activeUser: {
+				name: "Maximilian Schwarzmüller",
+				description: "Site owner and admin",
+				role: "admin",
+			},
+		};
+	},
 };
 </script>
 
-<style>
+<style scoped>
 html {
-  font-family: sans-serif;
+	font-family: sans-serif;
 }
 
 body {
-  margin: 0;
+	margin: 0;
 }
 </style>
